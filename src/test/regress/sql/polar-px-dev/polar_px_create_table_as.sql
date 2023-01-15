435d2ee5e77260dee9dad5ddad2982fab7191f72
棠羽 <mrdrivingduck@gmail.com>
2023-01-15 17:36:39 +0800

    bug: PX create table as fallback with oids

--- src/test/regress/sql/polar-px-dev/polar_px_create_table_as.sql
+++ src/test/regress/sql/polar-px-dev/polar_px_create_table_as.sql
@@ -139,6 +139,39 @@ select count(*) from heap_page_items(get_raw_page('px_mv_1', 2)) as a, heap_page
 select count(*) from heap_page_items(get_raw_page('px_mv_1', 3)) as a, heap_page_items(get_raw_page('px_mv_2', 3)) as b where a.lp = b.lp and a.t_data = b.t_data;
 select count(*) from heap_page_items(get_raw_page('px_mv_1', 4)) as a, heap_page_items(get_raw_page('px_mv_2', 4)) as b where a.lp = b.lp and a.t_data = b.t_data;
 
+-- create table as with oid (fall back from PX)
+explain create table ctas with oids as select * from px_test;
+create table ctas with oids as select * from px_test;
+drop table ctas;
+
+-- create table as with a table with oids
+create table t_withoids (id int) with oids;
+insert into t_withoids select generate_series(1,1000);
+
+explain create table ctas_1 as select * from t_withoids;
+create table ctas_1 as select * from t_withoids;
+
+set polar_px_enable_create_table_as = 0;
+explain create table ctas_2 as select * from t_withoids;
+create table ctas_2 as select * from t_withoids;
+set polar_px_enable_create_table_as = 1;
+
+select count(*) from heap_page_items(get_raw_page('ctas_1', 0)) as a, heap_page_items(get_raw_page('ctas_2', 0)) as b where a.lp = b.lp and a.t_data = b.t_data;
+select count(*) from heap_page_items(get_raw_page('ctas_1', 1)) as a, heap_page_items(get_raw_page('ctas_2', 1)) as b where a.lp = b.lp and a.t_data = b.t_data;
+select count(*) from heap_page_items(get_raw_page('ctas_1', 2)) as a, heap_page_items(get_raw_page('ctas_2', 2)) as b where a.lp = b.lp and a.t_data = b.t_data;
+select count(*) from heap_page_items(get_raw_page('ctas_1', 3)) as a, heap_page_items(get_raw_page('ctas_2', 3)) as b where a.lp = b.lp and a.t_data = b.t_data;
+select count(*) from heap_page_items(get_raw_page('ctas_1', 4)) as a, heap_page_items(get_raw_page('ctas_2', 4)) as b where a.lp = b.lp and a.t_data = b.t_data;
+
+drop table ctas_1;
+drop table ctas_2;
+drop table t_withoids;
+
+-- create table as without oid
+explain create table ctas as select * from px_test;
+create table ctas as select * from px_test;
+drop table ctas;
+
+-- reset GUCs
 set polar_enable_create_table_as_bulk_insert = 0;
 set polar_px_enable_create_table_as = 0;
 set polar_enable_px = off;
