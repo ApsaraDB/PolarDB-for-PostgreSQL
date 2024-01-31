3918995f6003c4cbb24f91c8bfedcad506b6074f
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-01-31 11:40:29 +0200

    Remove some obsolete smgrcloseall() calls.
    Before the advent of PROCSIGNAL_BARRIER_SMGRRELEASE, we didn't have a
    comprehensive way to deal with Windows file handles that get in the way
    of unlinking directories.  We had smgrcloseall() calls in a few places
    to try to mitigate.
    
    It's still a good idea for bgwriter and checkpointer to do that once per
    checkpoint so they don't accumulate unbounded SMgrRelation objects, but
    there is no longer any reason to close them at other random places such
    as the error path, and the explanation as given in the comments is now
    obsolete.
    
    Author: Thomas Munro
    Reviewed-by: Heikki Linnakangas, Robert Haas
    Discussion: https://www.postgresql.org/message-id/CA%2BhUKGJ8NTvqLHz6dqbQnt2c8XCki4r2QvXjBQcXpVwxTY_pvA@mail.gmail.com

--- src/backend/postmaster/polar_parallel_bgwriter.c
+++ src/backend/postmaster/polar_parallel_bgwriter.c
@@ -372,13 +372,6 @@ polar_parallel_bgwriter_worker_main(Datum main_arg)
 		 */
 		pg_usleep(1000000L);
 
-		/*
-		 * Close all open files after any error.  This is helpful on Windows,
-		 * where holding deleted files open causes various strange errors.
-		 * It's not clear we need it elsewhere, but shouldn't hurt.
-		 */
-		smgrcloseall();
-
 		/* Report wait end here, when there is no further possibility of wait */
 		pgstat_report_wait_end();
 	}
