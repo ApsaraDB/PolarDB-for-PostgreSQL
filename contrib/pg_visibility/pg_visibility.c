9c759a7f3e239ec8e6f68c4af75a77b433e96792
mrdrivingduck <mrdrivingduck@gmail.com>
2025-03-05 14:48:18 +0800

    feat: refactor smgrnblocks for smgrtruncate2

--- contrib/pg_visibility/pg_visibility.c
+++ contrib/pg_visibility/pg_visibility.c
@@ -400,14 +400,7 @@ pg_truncate_visibility_map(PG_FUNCTION_ARGS)
 	/* Compute new and old size before entering critical section. */
 	fork = VISIBILITYMAP_FORKNUM;
 	block = visibilitymap_prepare_truncate(rel, 0);
-	old_block = BlockNumberIsValid(block) ? smgrnblocks(RelationGetSmgr(rel), fork) : 0;
-
-	/*
-	 * POLAR RSC: make sure all segments are opened.
-	 */
-	if (POLAR_RSC_SHOULD_UPDATE(RelationGetSmgr(rel), fork) && BlockNumberIsValid(block))
-		(void) smgrnblocks_real(RelationGetSmgr(rel), fork);
-	/* POLAR end */
+	old_block = BlockNumberIsValid(block) ? smgrnblocks_ensure_opened(RelationGetSmgr(rel), fork) : 0;
 
 	/*
 	 * WAL-logging, buffer dropping, file truncation must be atomic and all on
