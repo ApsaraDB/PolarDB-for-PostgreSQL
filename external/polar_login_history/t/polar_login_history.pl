031c90032c7d8e4554b84af784aa75ce5ef45665
Shijun.Zhou <manfuldrifter@hotmail.com>
2025-03-20 18:45:37 +0800

    feat: security feature - login history
    Adjust the format and add the test case

--- external/polar_login_history/t/polar_login_history.pl
+++ external/polar_login_history/t/polar_login_history.pl
@@ -11,25 +11,91 @@ use PostgreSQL::Test::Cluster;
 use PostgreSQL::Test::Utils;
 use Test::More;
 
-my $node = PostgreSQL::Test::Cluster->new('primary');
-$node->init();
-$node->start;
-
-$node->append_conf('postgresql.conf',
+############### init primary/replica/standby ##############
+my $node_primary = PostgreSQL::Test::Cluster->new('primary');
+$node_primary->polar_init_primary;
+$node_primary->append_conf('postgresql.conf',
 	"shared_preload_libraries = 'polar_login_history'");
-$node->restart;
 
-$node->append_conf('postgresql.conf', "polar_login_history.enable = on");
-$node->restart;
+my $node_replica = PostgreSQL::Test::Cluster->new('replica');
+$node_replica->polar_init_replica($node_primary);
 
-is($node->safe_psql('postgres', 'show polar_login_history.enable;'),
-	'on', 'login history function is enabled');
+my $node_standby = PostgreSQL::Test::Cluster->new('standby');
+$node_standby->polar_init_standby($node_primary);
 
-$node->safe_psql('postgres', 'create user zhangsan;');
-is($node->psql('postgres', undef, extra_params => [ '-U', 'zhangsan' ]),
-	0, 'login success');
-$node->safe_psql('postgres', 'drop user zhangsan;');
+$node_primary->start;
 
-$node->stop;
+$node_primary->polar_create_slot($node_replica->name);
+$node_primary->polar_create_slot($node_standby->name);
 
+$node_replica->start;
+$node_standby->start;
+
+$node_standby->polar_drop_all_slots;
+
+
+############### enable login history ##############
+$node_primary->append_conf('postgresql.conf',
+	"polar_login_history.enable = on");
+$node_primary->restart;
+is( $node_primary->safe_psql('postgres', 'show polar_login_history.enable;'),
+	'on',
+	'login history function is enabled');
+
+$node_replica->append_conf('postgresql.conf',
+	"polar_login_history.enable = on");
+$node_replica->restart;
+is( $node_replica->safe_psql('postgres', 'show polar_login_history.enable;'),
+	'on',
+	'login history function is enabled');
+
+$node_standby->append_conf('postgresql.conf',
+	"polar_login_history.enable = on");
+$node_standby->restart;
+is( $node_standby->safe_psql('postgres', 'show polar_login_history.enable;'),
+	'on',
+	'login history function is enabled');
+
+
+############### log in to the database with the new user ##############
+$node_primary->safe_psql('postgres', 'create user zhangsan;');
+$node_replica->restart;
+
+is( $node_primary->psql(
+		'postgres', undef, extra_params => [ '-U', 'zhangsan' ]),
+	0,
+	'login success');
+isnt(
+	$node_primary->psql(
+		'postgres1', undef, extra_params => [ '-U', 'zhangsan' ]),
+	0,
+	'login success');
+
+is( $node_replica->psql(
+		'postgres', undef, extra_params => [ '-U', 'zhangsan' ]),
+	0,
+	'login success');
+isnt(
+	$node_replica->psql(
+		'postgres1', undef, extra_params => [ '-U', 'zhangsan' ]),
+	0,
+	'login success');
+
+is( $node_standby->psql(
+		'postgres', undef, extra_params => [ '-U', 'zhangsan' ]),
+	0,
+	'login success');
+isnt(
+	$node_standby->psql(
+		'postgres1', undef, extra_params => [ '-U', 'zhangsan' ]),
+	0,
+	'login success');
+
+$node_primary->safe_psql('postgres', 'drop user zhangsan;');
+
+
+############### stop ##############
+$node_primary->stop;
+$node_replica->stop;
+$node_standby->stop;
 done_testing();
