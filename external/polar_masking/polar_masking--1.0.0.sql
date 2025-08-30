353c37e78ad144795fdf42df128578ad2ee75162
Muyijia <yang.jia3@zte.com.cn>
2025-08-30 09:51:01 +0800

    feat: data masking (masking operator)
    This commit provides functions of polar masking:
    - sql functions of masking operators

--- external/polar_masking/polar_masking--1.0.0.sql
+++ external/polar_masking/polar_masking--1.0.0.sql
@@ -45,3 +45,101 @@ VOLATILE LANGUAGE C AS 'MODULE_PATHNAME', 'polar_masking_alter_label_maskingop';
 
 CREATE FUNCTION polar_masking.polar_masking_alter_label_maskingop_set_regexpmasking(label_name text, start_pos Integer, end_pos Integer, regex text, replace_text text) RETURNS VOID
 VOLATILE LANGUAGE C AS 'MODULE_PATHNAME', 'polar_masking_alter_label_maskingop_set_regexpmasking';
+
+CREATE OR REPLACE FUNCTION polar_masking.creditcardmasking(col text, letter char default 'x') RETURNS text AS $$
+declare 
+    size INTEGER := 4;
+begin
+    return CASE WHEN pg_catalog.length(col) >= size THEN
+        pg_catalog.REGEXP_REPLACE(pg_catalog.left(col, size*(-1)), '[\d+]', letter, 'g') || pg_catalog.right(col, size)
+    ELSE
+        col
+    end;
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.basicemailmasking(col text, letter char default 'x') RETURNS text AS $$
+declare 
+    pos INTEGER := position('@' in col);
+begin
+    return CASE WHEN pos > 1 THEN
+    pg_catalog.repeat(letter, pos - 1) || pg_catalog.substring(col, pos, pg_catalog.length(col) - pos +1)
+    ELSE
+        col
+    end;
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.fullemailmasking(col text, letter char default 'x') RETURNS text AS $$
+declare 
+    pos INTEGER := position('@' in col);
+    dot_pos INTEGER := pg_catalog.length(col) - position('.' in pg_catalog.reverse(col)) + 1;
+    d_pos INTEGER := position('.' in pg_catalog.reverse(col));
+begin
+    return CASE WHEN pos > 0 and dot_pos > pos and d_pos > 0 THEN
+    pg_catalog.repeat(letter, pos - 1) || '@' || pg_catalog.repeat(letter,  dot_pos - pos - 1) || pg_catalog.substring(col, dot_pos, pg_catalog.length(col) - dot_pos +1)
+    WHEN pos = 0 and d_pos > 0 THEN
+    pg_catalog.repeat(letter,  dot_pos - 1) || pg_catalog.substring(col, dot_pos, pg_catalog.length(col) - dot_pos +1)
+    ELSE
+        col
+    end;
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.alldigitsmasking(col text, letter char default '0') RETURNS text AS $$
+begin
+    return pg_catalog.REGEXP_REPLACE(col, '[\d+]', letter, 'g');
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.shufflemasking(col text) RETURNS text AS $$
+declare 
+    rd INTEGER;
+    size INTEGER := pg_catalog.length(col);
+    tmp text := col;
+    res text := '';
+begin
+    while size > 0 loop 
+        rd := pg_catalog.floor(pg_catalog.random() * pg_catalog.length(tmp) + 1);
+        res := res || pg_catalog.right(pg_catalog.left(tmp, rd), 1);
+        tmp := pg_catalog.left(tmp, rd - 1) || pg_catalog.right(tmp, pg_catalog.length(tmp) - rd);
+        size := size - 1;
+    END loop;
+    return res;
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.randommasking(col text) RETURNS text AS $$	
+begin
+    return pg_catalog.left(pg_catalog.MD5(pg_catalog.random()::text), pg_catalog.length(col));
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.maskall(col text, letter char default 'x') RETURNS text AS $$
+declare 
+    size INTEGER := pg_catalog.length(col);
+begin
+    return pg_catalog.repeat(letter, size);
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION polar_masking.regexpmasking(col text, reg text, replace_text text, start_pos INTEGER default 0, end_pos INTEGER default 0) RETURNS text AS $$
+declare
+	size INTEGER := pg_catalog.length(col);
+	endpos INTEGER := end_pos;
+	startpos INTEGER := start_pos;
+	lstr text;
+	rstr text;
+	ltarget text;
+begin
+	IF startpos <= 0 THEN startpos := 1; END IF;
+	IF startpos > size THEN startpos := size + 1; END IF;
+	IF endpos <= 0 THEN endpos := size; END IF;
+	IF endpos > size THEN endpos := size; END IF;
+    lstr := pg_catalog.left(col, startpos - 1);
+    rstr := pg_catalog.right(col, size - endpos);
+    ltarget := pg_catalog.substring(col, startpos, endpos - startpos+ 1);
+    ltarget := pg_catalog.regexp_replace(ltarget, reg, replace_text, 'g');
+    return lstr || ltarget || rstr;
+end;
+$$ LANGUAGE plpgsql;
